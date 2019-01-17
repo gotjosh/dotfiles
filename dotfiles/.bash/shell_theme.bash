73abcdcfbf045117797edf0722c9cf208c9c8032
gotjosh <josue.abreu@gmail.com>
2019-01-17 10:12:45 +0000

    Setup LSCOLORS

--- dotfiles/.bash/shell_theme.bash
+++ dotfiles/.bash/shell_theme.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
- export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ "
+export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced

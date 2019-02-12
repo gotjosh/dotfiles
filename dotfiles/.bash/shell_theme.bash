7f4d84ea8f89ef07145d280fd8f953f2f5893d11
gotjosh <josue.abreu@gmail.com>
2019-02-12 10:41:23 +0000

    Include the git branch as part of my shell theme

--- dotfiles/.bash/shell_theme.bash
+++ dotfiles/.bash/shell_theme.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
-export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ "
+export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$(parse_git_branch)\[\033[00m\] $ "
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}

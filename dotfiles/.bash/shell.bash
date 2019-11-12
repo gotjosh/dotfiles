78aa94e1b6186d0a2cb5778427c6b1f20a07d423
gotjosh <josue@grafana.com>
2019-11-12 14:36:07 +0000

    Bump the bash history entries to 50k from 10k

--- dotfiles/.bash/shell.bash
+++ dotfiles/.bash/shell.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Store 10,000 history entries
-export HISTSIZE=10000
+# Store 50,000 history entries
+export HISTSIZE=50000
 # Don't store duplicates
 export HISTCONTROL=erasedups
 # Append to history file
